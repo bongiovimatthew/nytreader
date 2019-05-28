649009037f2e25b32f876f22f325b6580e0affdb
Matt Bongiovi <bongiovimatthew@gmail.com>
2019-05-28 14:49:28 -0700

    Add support for article search API

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import NYTAPI from './services/nytquery';
 // import List from '@material-ui/core/List';
 // import ListItem from '@material-ui/core/ListItem';
 // import ListItemText from '@material-ui/core/ListItemText';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
-      slugs: []
+      slugs: [],
+      articlesToLoad: 0,
+      isFetching: true
     };
   }
 
@@ -19,34 +22,100 @@ class App extends React.Component {
   }
 
   getData() {
-    this.moreDataCallback = this.moreDataCallback.bind(this);
-    NYTAPI.beginGetData(this.moreDataCallback);
+    // this.moreDataCallback = this.moreDataCallback.bind(this);
+    // NYTAPI.beginGetData(this.moreDataCallback);
+
+    this.moreDataCallback_archive = this.moreDataCallback_archive.bind(this);
+    NYTAPI.beginGetData_archive(this.moreDataCallback_archive);
   };
 
-  moreDataCallback(data){
-    const simpleData = data.map((item, index) => ({
+  async getArticleData(url){
+    // const response = await fetch(url, {mode: "no-cors"});
+    // console.log("Fetching article HTML");
+    // console.log(response);
+  };
+
+  moreDataCallback_archive(data, pagesRemaining){
+    if (!data) return;
+
+    this.setState({ articlesToLoad: data.meta.hits });
+
+    if (pagesRemaining > 0){
+      this.setState({ isFetching: true });
+    }else{
+      this.setState({isFetching: false});
+    }
+
+    const simpleData = data.docs.map((item, index) => ({
+      key: `${index}`,
+      slug: `${item._id}`,
+      url: `${item.web_url}`,
+      published: new Date(item.pub_date),
+      title: `${item.headline.main}`,
+      byline: item.byline.original == null ? "" : `${item.byline.original}`,
+      subheadline: `${item.abstract}`,
+      item_type: `${item.document_type}`,
+      wordCount: `${item.word_count}`
+    }));
+
+    const tempData = this.state.data;
+    const tempSlugs = this.state.slugs;
+    simpleData.forEach(element => {
+      if (!(tempSlugs.includes(element.slug)) && element.url){
+        tempSlugs.push(element.slug);
+        tempData.push(element);
+      }
+    });
+
+    tempData.sort(function(a, b) {
+      var x = a.published; var y = b.published;
+      return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+    });
+
+    this.setState({ data: tempData, slugs: tempSlugs });
+  };
+
+  moreDataCallback(data, pagesRemaining){
+    if (!data) return;
+
+    this.setState({ articlesToLoad: data.num_results });
+
+    if (pagesRemaining > 0){
+      this.setState({ isFetching: true });
+    }else{
+      this.setState({isFetching: false});
+    }
+
+    const simpleData = data.results.map((item, index) => ({
       key: `${index}`,
       slug: `${item.slug_name}`,
       url: `${item.url}`,
-      published: `${item.published_date}`,
+      published: new Date(item.first_published_date),
       title: `${item.title}`,
       byline: `${item.byline}`,
       subheadline: `${item.subheadline}`,
       item_type: `${item.item_type}`
     }));
 
-    let temp = this.state.data;
-    simpleData.map(item => {
-      if(!(item in temp)) {
-        return temp.push(item);
+    const tempData = this.state.data;
+    const tempSlugs = this.state.slugs;
+    simpleData.forEach(element => {
+      if (!(tempSlugs.includes(element.slug))){
+        tempSlugs.push(element.slug);
+        tempData.push(element);
       }
-      return null;
+
+      if (element.slug === "Cockburn"){
+        this.getArticleData(element.url);
+      }
     });
 
-    this.setState(previousState => ({
-      data: temp.concat(previousState.data),
-    }));
+    tempData.sort(function(a, b) {
+      var x = a.published; var y = b.published;
+      return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+    });
 
+    this.setState({ data: tempData, slugs: tempSlugs });
   }
 
   handleListItemClick(event, index){
@@ -55,16 +124,25 @@ class App extends React.Component {
   
   render() {
     return (<div className="App">
-      <header className="App-header">
+      <div className="Loading">
+        <div>Total Articles: {this.state.articlesToLoad}</div>
+        <div>Articles Loaded: {this.state.slugs.length}</div>
+        {this.state.isFetching && <CircularProgress />}
+      </div>     
       <ul className="Article-list">
       {this.state.data.map((item, index) => (
         <li className="Article-item" key={index}>
-          <a className="Article-item" href={item.url}>{item.title}</a>
+          {index+1}
+
+          <a className="Article-item" href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
+          {item.published.toString()}
+          <br />
           {item.byline}
+          <br />
+          {item.wordCount + ' words (about ' + Math.ceil(item.wordCount / 225) + ' minutes)'}
         </li>
       ))}
       </ul>
-      </header>
     </div>);
   }
 }
--- src/services/nytquery.js
+++ src/services/nytquery.js
@@ -1,4 +1,26 @@
 class NYTAPI {
+
+    static async beginGetData_archive(dataCallback){
+        
+        let numResults = 0;
+        const firstResponse = await NYTAPI.getOffsetData_archive(0);
+
+        if(firstResponse.status === "OK"){
+            numResults = firstResponse.response.meta.hits;
+            console.log(`NumResults: ${numResults}`);
+            const pages = Math.ceil(numResults / 10.0);
+
+            dataCallback(firstResponse.response, pages - 1);
+            console.log(`numpages: ${pages}`);
+
+            for (var i=1; i < pages; i += 1) {
+                const data = await NYTAPI.getOffsetData_archive(i);
+                dataCallback(data.response, pages - 1 - i);
+                console.log(data);                
+            }
+        }
+    };
+
     static async beginGetData(dataCallback) {
         
         let numResults = 0;
@@ -7,30 +29,35 @@ class NYTAPI {
         if(firstResponse.status === "OK"){
             numResults = firstResponse.num_results;
             console.log(`NumResults: ${numResults}`);
-            dataCallback(firstResponse.results)
-
-            console.log(firstResponse.results);
-
             const pages = Math.ceil(numResults / 20.0);
+
+            dataCallback(firstResponse, pages - 1);
             console.log(`numpages: ${pages}`);
 
             for (var i=1; i < pages; i += 1) {
                 const data = await NYTAPI.getOffsetData(i);
-                dataCallback(data.results);
+                dataCallback(data, pages - 1 - i);
                 console.log(data);                
             }
-
-            /* [...Array(pages).keys()].map((item, index) => {
-                return NYTAPI.getOffsetData(index+1).then(results => {
-                    dataCallback(results.results);
-                    console.log(results);
-                });
-            });*/
         }
     };
 
     static sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    static async getOffsetData_archive(offset){
+        await NYTAPI.sleep(6000);
+        console.log(`getOffset_archive: ${offset}`);
+    
+        const end = new Date();
+        const startStr = `${end.getFullYear()}${("0" + (end.getMonth() + 1)).slice(-2)}${end.getDate()}`
+
+        const apiKey = process.env.REACT_APP_NYT_API_KEY;
+        const requestUri = `https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=${startStr}&end_date=${startStr}&api-key=${apiKey}&fq=source:("The New York Times")&page=${offset}`;
+        const response = await fetch(requestUri);
+        console.log(`Request: ${requestUri}`);
+        return response.json();
     }
 
     static async getOffsetData(offset){
@@ -40,7 +67,6 @@ class NYTAPI {
         const section = 'all';
         const timePeriod = 24;
     
-        // TODO: move to .env
         const apiKey = process.env.REACT_APP_NYT_API_KEY;
         const requestUri = `https://api.nytimes.com/svc/news/v3/content/${source}/${section}/${timePeriod}.json?api-key=${apiKey}&limit=20&offset=${offset}`;
         const response = await fetch(requestUri);
